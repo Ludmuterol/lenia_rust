8ccba784ca32c0c7805b3edb64461af5515125f3
Ludmuterol <45458713+Ludmuterol@users.noreply.github.com>
2022-09-04 02:02:29 +0200

    fft instead of convolution

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ extern crate sdl2;
 use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-const WIDTH: u32 = 500;
-const HEIGHT: u32 = 500;
-const PIXEL_EDGE_SIZE: u32 = 2;
+use fft2d::slice::{fft_2d, fftshift, ifft_2d};
+use num_complex::Complex;
+
+const WIDTH: u32 = 750;
+const HEIGHT: u32 = 750;
+const PIXEL_EDGE_SIZE: u32 = 1;
 
 const UPDATE_FREQ: f64 = 10.;
 const KERNEL_RAD: u32 = 13;
@@ -15,8 +18,6 @@ const BELL_M: f64 = 0.15;
 const BELL_S: f64 = 0.015;
 
 //calculated at compiletime
-const KERNEL_SIZE: usize = (2 * KERNEL_RAD + 1) as usize;
-const KERNEL_TOT: usize = KERNEL_SIZE * KERNEL_SIZE;
 const A_WIDTH: u32 = WIDTH / PIXEL_EDGE_SIZE;
 const A_HEIGHT: u32 = HEIGHT / PIXEL_EDGE_SIZE;
 const A_SIZE: usize = (A_WIDTH * A_HEIGHT) as usize;
@@ -24,7 +25,7 @@ const A_SIZE: usize = (A_WIDTH * A_HEIGHT) as usize;
 trait Sum<T> {
     fn sum(&self) -> T;
 }
-impl<T: std::convert::From<i32> + for<'a> std::ops::AddAssign<&'a T> + 'static, const N: usize> Sum<T> for [T; N] {
+impl<T: std::convert::From<i32> + for<'a> std::ops::AddAssign<&'a T> + 'static> Sum<T> for Vec<T> {
     fn sum(&self) -> T{
         let mut sum:T = 0.into();
         for i in self.iter(){
@@ -42,30 +43,6 @@ fn bell(x: f64, m: f64, s: f64) -> f64 {
     f64::exp(-((x - m)/s).powi(2) / 2.)
 }
 
-fn convolve2d(image: &[f64], kernel: &[f64]) -> Vec<f64> {
-    let mut ret = image.to_vec();
-    for i in 0..A_HEIGHT {
-        for j in 0..A_WIDTH {
-            ret[(i * A_WIDTH + j) as usize] = {
-                let mut sum: f64 = 0.;
-                for ik in 0..KERNEL_SIZE {
-                    for jk in 0..KERNEL_SIZE {
-                        let mut mv_i = i as i32 + ik as i32 - KERNEL_RAD as i32;
-                        let mut mv_j = j as i32 + jk as i32 - KERNEL_RAD as i32;
-                        if mv_i < 0 {mv_i += A_HEIGHT as i32;}
-                        if mv_j < 0 {mv_j += A_WIDTH as i32;}
-                        if mv_i >= A_HEIGHT as i32 {mv_i -= A_HEIGHT as i32;}
-                        if mv_j >= A_WIDTH as i32 {mv_j -= A_WIDTH as i32;}
-                        sum += image[(mv_i * A_WIDTH as i32 + mv_j) as usize] * kernel[((KERNEL_SIZE - ik - 1) * KERNEL_SIZE + (KERNEL_SIZE - jk - 1)) as usize];
-                    }
-                }
-                sum
-            }
-        }
-    }
-    ret
-}
-
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -80,59 +57,69 @@ fn main() {
     let mut texture = texture_creator.create_texture_streaming(sdl2::pixels::PixelFormatEnum::ARGB8888, WIDTH, HEIGHT).unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let mut pxl_vec = vec![0.; A_SIZE];
 	//"name":"Orbium","R":13,"T":10,"m":0.15,"s":0.015,"b":[1] widt = 20 height = 20
-	let orbium = [0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.1 ,0.14,0.1 ,0.  ,0.  ,0.03,0.03,0.  ,0.  ,0.3 ,0.  ,0.  ,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.  ,0.08,0.24,0.3 ,0.3 ,0.18,0.14,0.15,0.16,0.15,0.09,0.2 ,0.  ,0.  ,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.  ,0.15,0.34,0.44,0.46,0.38,0.18,0.14,0.11,0.13,0.19,0.18,0.45,0.  ,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.06,0.13,0.39,0.5 ,0.5 ,0.37,0.06,0.  ,0.  ,0.  ,0.02,0.16,0.68,0.  ,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.11,0.17,0.17,0.33,0.4 ,0.38,0.28,0.14,0.  ,0.  ,0.  ,0.  ,0.  ,0.18,0.42,0.  ,0.  ,
-                  0.  ,0.  ,0.09,0.18,0.13,0.06,0.08,0.26,0.32,0.32,0.27,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.82,0.  ,0.  ,
-                  0.27,0.  ,0.16,0.12,0.  ,0.  ,0.  ,0.25,0.38,0.44,0.45,0.34,0.  ,0.  ,0.  ,0.  ,0.  ,0.22,0.17,0.  ,
-                  0.  ,0.07,0.2 ,0.02,0.  ,0.  ,0.  ,0.31,0.48,0.57,0.6 ,0.57,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.49,0.  ,
-                  0.  ,0.59,0.19,0.  ,0.  ,0.  ,0.  ,0.2 ,0.57,0.69,0.76,0.76,0.49,0.  ,0.  ,0.  ,0.  ,0.  ,0.36,0.  ,
-                  0.  ,0.58,0.19,0.  ,0.  ,0.  ,0.  ,0.  ,0.67,0.83,0.9 ,0.92,0.87,0.12,0.  ,0.  ,0.  ,0.  ,0.22,0.07,
-                  0.  ,0.  ,0.46,0.  ,0.  ,0.  ,0.  ,0.  ,0.7 ,0.93,1.  ,1.  ,1.  ,0.61,0.  ,0.  ,0.  ,0.  ,0.18,0.11,
-                  0.  ,0.  ,0.82,0.  ,0.  ,0.  ,0.  ,0.  ,0.47,1.  ,1.  ,0.98,1.  ,0.96,0.27,0.  ,0.  ,0.  ,0.19,0.1 ,
-                  0.  ,0.  ,0.46,0.  ,0.  ,0.  ,0.  ,0.  ,0.25,1.  ,1.  ,0.84,0.92,0.97,0.54,0.14,0.04,0.1 ,0.21,0.05,
-                  0.  ,0.  ,0.  ,0.4 ,0.  ,0.  ,0.  ,0.  ,0.09,0.8 ,1.  ,0.82,0.8 ,0.85,0.63,0.31,0.18,0.19,0.2 ,0.01,
-                  0.  ,0.  ,0.  ,0.36,0.1 ,0.  ,0.  ,0.  ,0.05,0.54,0.86,0.79,0.74,0.72,0.6 ,0.39,0.28,0.24,0.13,0.  ,
-                  0.  ,0.  ,0.  ,0.01,0.3 ,0.07,0.  ,0.  ,0.08,0.36,0.64,0.7 ,0.64,0.6 ,0.51,0.39,0.29,0.19,0.04,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.1 ,0.24,0.14,0.1 ,0.15,0.29,0.45,0.53,0.52,0.46,0.4 ,0.31,0.21,0.08,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.  ,0.08,0.21,0.21,0.22,0.29,0.36,0.39,0.37,0.33,0.26,0.18,0.09,0.  ,0.  ,0.  ,
-                  0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.03,0.13,0.19,0.22,0.24,0.24,0.23,0.18,0.13,0.05,0.  ,0.  ,0.  ,0.  ,
-	              0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.02,0.06,0.08,0.09,0.07,0.05,0.01,0.  ,0.  ,0.  ,0.  ,0.  ];
+	let orbium = [0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.1 ,0.14,0.1 ,0.  ,0.  ,0.03,0.03,0.  ,0.  ,0.3 ,0.  ,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.  ,0.08,0.24,0.3 ,0.3 ,0.18,0.14,0.15,0.16,0.15,0.09,0.2 ,0.  ,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.  ,0.15,0.34,0.44,0.46,0.38,0.18,0.14,0.11,0.13,0.19,0.18,0.45,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.06,0.13,0.39,0.5 ,0.5 ,0.37,0.06,0.  ,0.  ,0.  ,0.02,0.16,0.68,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.11,0.17,0.17,0.33,0.4 ,0.38,0.28,0.14,0.  ,0.  ,0.  ,0.  ,0.  ,0.18,0.42,0.  ,0.  , 
+                  0.  ,0.  ,0.09,0.18,0.13,0.06,0.08,0.26,0.32,0.32,0.27,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.82,0.  ,0.  , 
+                  0.27,0.  ,0.16,0.12,0.  ,0.  ,0.  ,0.25,0.38,0.44,0.45,0.34,0.  ,0.  ,0.  ,0.  ,0.  ,0.22,0.17,0.  , 
+                  0.  ,0.07,0.2 ,0.02,0.  ,0.  ,0.  ,0.31,0.48,0.57,0.6 ,0.57,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.49,0.  , 
+                  0.  ,0.59,0.19,0.  ,0.  ,0.  ,0.  ,0.2 ,0.57,0.69,0.76,0.76,0.49,0.  ,0.  ,0.  ,0.  ,0.  ,0.36,0.  , 
+                  0.  ,0.58,0.19,0.  ,0.  ,0.  ,0.  ,0.  ,0.67,0.83,0.9 ,0.92,0.87,0.12,0.  ,0.  ,0.  ,0.  ,0.22,0.07, 
+                  0.  ,0.  ,0.46,0.  ,0.  ,0.  ,0.  ,0.  ,0.7 ,0.93,1.  ,1.  ,1.  ,0.61,0.  ,0.  ,0.  ,0.  ,0.18,0.11, 
+                  0.  ,0.  ,0.82,0.  ,0.  ,0.  ,0.  ,0.  ,0.47,1.  ,1.  ,0.98,1.  ,0.96,0.27,0.  ,0.  ,0.  ,0.19,0.1 , 
+                  0.  ,0.  ,0.46,0.  ,0.  ,0.  ,0.  ,0.  ,0.25,1.  ,1.  ,0.84,0.92,0.97,0.54,0.14,0.04,0.1 ,0.21,0.05, 
+                  0.  ,0.  ,0.  ,0.4 ,0.  ,0.  ,0.  ,0.  ,0.09,0.8 ,1.  ,0.82,0.8 ,0.85,0.63,0.31,0.18,0.19,0.2 ,0.01, 
+                  0.  ,0.  ,0.  ,0.36,0.1 ,0.  ,0.  ,0.  ,0.05,0.54,0.86,0.79,0.74,0.72,0.6 ,0.39,0.28,0.24,0.13,0.  , 
+                  0.  ,0.  ,0.  ,0.01,0.3 ,0.07,0.  ,0.  ,0.08,0.36,0.64,0.7 ,0.64,0.6 ,0.51,0.39,0.29,0.19,0.04,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.1 ,0.24,0.14,0.1 ,0.15,0.29,0.45,0.53,0.52,0.46,0.4 ,0.31,0.21,0.08,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.  ,0.08,0.21,0.21,0.22,0.29,0.36,0.39,0.37,0.33,0.26,0.18,0.09,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.03,0.13,0.19,0.22,0.24,0.24,0.23,0.18,0.13,0.05,0.  ,0.  ,0.  ,0.  , 
+                  0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.  ,0.02,0.06,0.08,0.09,0.07,0.05,0.01,0.  ,0.  ,0.  ,0.  ,0.  ];
+
+    let mut pxl_vec = vec![0.; A_SIZE];
     for i in 0..20 {
         for j in 0..20 {
             pxl_vec[(i * A_WIDTH + j) as usize] = orbium[(i * 20 + j) as usize];
         }
     }
-    //let mut rng = rand::thread_rng();
-    //for i in pxl_vec.iter_mut(){
-    //    *i = rng.gen();
-    //}
-    let kern_stp: [f64; KERNEL_TOT] = {
-        let mut x = [0.; KERNEL_TOT];
-        for i in 0..KERNEL_SIZE {
-            for j in 0..KERNEL_SIZE {
-                let tmp_val = (
-                     (i as f64 - KERNEL_RAD as f64).powi(2) +
-                     (j as f64 - KERNEL_RAD as f64).powi(2) ).sqrt() / KERNEL_RAD as f64;
-                x[(i * KERNEL_SIZE + j)] = {
-                    if tmp_val < 1.0 {bell(tmp_val, 0.5, 0.15)}
-                    else {0.}
-                };
-            }
-        }
-        x
-    };
+    let mut rng = rand::thread_rng();
+    for i in pxl_vec.iter_mut(){
+        *i = rng.gen();
+    }
+	let mut kern_stp: Vec<f64> = vec![0.; A_SIZE];
+	for (i, row) in kern_stp.chunks_exact_mut(A_WIDTH as usize).enumerate() {
+		for (j, pix) in row.iter_mut().enumerate() {
+			let tmp_val = (
+				(i as f64 - (A_HEIGHT / 2) as f64).powi(2) + 
+				(j as f64 - (A_WIDTH / 2 ) as f64).powi(2) 
+			).sqrt() / KERNEL_RAD as f64;
+			*pix = {
+				if tmp_val < 1.0 {bell(tmp_val, 0.5, 0.15)}
+				else {0.}
+			};
+			
+		}
+	}
+
     let sum: f64 = kern_stp.sum();
-    let kernel = kern_stp.map(|x| x / sum);
+	let mut comp_kern: Vec<Complex<f64>> = kern_stp.iter().map(|&x| Complex::new(x / sum, 0.0) ).collect();
+    comp_kern = fftshift(A_WIDTH as usize, A_HEIGHT as usize, &comp_kern);
+	fft_2d(A_WIDTH as usize, A_HEIGHT as usize, &mut comp_kern);
+
     let colorgrad = colorgrad::viridis();
     'running: loop {
-        let result = convolve2d(&pxl_vec, &kernel);
-		for (v, c) in pxl_vec.iter_mut().zip(result) {
-			*v = (*v + ((1. / UPDATE_FREQ) * growth(c))).clamp(0. , 1.);
+		let start = Instant::now();
+        let mut image: Vec<Complex<f64>> = pxl_vec.iter().map(|&x| Complex::new(x, 0.0)).collect();
+		fft_2d(A_WIDTH as usize, A_HEIGHT as usize, &mut image);
+		for (i, k) in image.iter_mut().zip(&comp_kern) {
+			*i *= k;
+		}
+		ifft_2d(A_WIDTH as usize, A_HEIGHT as usize, &mut image);
+		for (v, c) in pxl_vec.iter_mut().zip(&image) {
+			*v = (*v + ((1. / UPDATE_FREQ) * growth((c * 1.0 / (A_WIDTH * A_HEIGHT) as f64).re))).clamp(0. , 1.);
 		}
         texture.with_lock(
             None,
@@ -142,7 +129,7 @@ fn main() {
                         let offset: usize = (i * WIDTH * 4 + j * 4) as usize;
                         // WINDOWS: BGRA (endianess)
                         let color = colorgrad.at(pxl_vec[
-                                                 ((i / PIXEL_EDGE_SIZE) * A_WIDTH
+                                                 ((i / PIXEL_EDGE_SIZE) * A_WIDTH 
                                                   + j / PIXEL_EDGE_SIZE) as usize] as f64).to_rgba8();
                         bytearray[offset    ] = color[2];
                         bytearray[offset + 1] = color[1];
@@ -164,7 +151,10 @@ fn main() {
             }
         }
         canvas.present();
-        ::std::thread::sleep(Duration::new(0,1_000_000_000u32 / 60));
+		let time = Duration::new(0,1_000_000_000u32 / 60).saturating_sub(start.elapsed());
+		if !time.is_zero() {
+        	::std::thread::sleep(time);
+		}
     }
 }
 
